d433d061101110ed50bc1b6268b6f4ec89c21348
Ken Dong <yid164@mail.usask.ca>
2021-06-15 15:28:29 -0600

    Initial Commit

---
+++ ios-photo-gallery/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios-photo-gallery
+//
+//  Created by Yinsheng Dong on 2021-06-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios-photo-gallery/ios_photo_galleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_photo_galleryApp.swift
+//  ios-photo-gallery
+//
+//  Created by Yinsheng Dong on 2021-06-15.
+//
+
+import SwiftUI
+
+@main
+struct ios_photo_galleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
