13f82670842274122d7ea2e4fe3988146ef8762d
Ken Dong <yid164@mail.usask.ca>
2021-06-16 22:06:38 -0600

    opt code

--- ios-photo-gallery/model/ImageModel.swift
+++ ios-photo-gallery/model/ImageModel.swift
@@ -14,7 +14,6 @@ struct ImageModel: Hashable {
     let afterImage: UIImage
     
     static var smaples: [ImageModel?] = [
-        nil,
         ImageModel(date: "April 10, 2021", beforeImage: UIImage(imageLiteralResourceName: "p1.HEIC"), afterImage: UIImage(imageLiteralResourceName: "p2.HEIC")),
         ImageModel(date: "April 11, 2021", beforeImage: UIImage(imageLiteralResourceName: "p3.HEIC"), afterImage: UIImage(imageLiteralResourceName: "p4.HEIC")),
         ImageModel(date: "April 12, 2021", beforeImage: UIImage(imageLiteralResourceName: "p5.HEIC"), afterImage: UIImage(imageLiteralResourceName: "p6.HEIC")),
@@ -25,6 +24,7 @@ struct ImageModel: Hashable {
         ImageModel(date: "April 17, 2021", beforeImage: UIImage(imageLiteralResourceName: "p15.JPG"), afterImage: UIImage(imageLiteralResourceName: "p16.HEIC")),
         ImageModel(date: "April 18, 2021", beforeImage: UIImage(imageLiteralResourceName: "p17.HEIC"), afterImage: UIImage(imageLiteralResourceName: "p18.HEIC")),
         ImageModel(date: "April 19, 2021", beforeImage: UIImage(imageLiteralResourceName: "p19.HEIC"), afterImage: UIImage(imageLiteralResourceName: "p20.HEIC")),
+        nil
     ]
     
     
